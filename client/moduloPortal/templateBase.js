34409a0f6d79e6f53cf6ff8f5cd4e17b3dcf9c97
nacho <cruzi3836@gmail.com>
2019-02-19 17:49:17 -0400

    tercera actualizacion

--- client/moduloPortal/templateBase.js
+++ client/moduloPortal/templateBase.js
@@ -1,4 +1,20 @@
 
 Template.templateBase.onRendered(function(){
 	$(".button-collapse").sideNav();
+	var user = {
+		"username" : 'ignacio',
+		"email" : 'cruzi3836@gmail.com',
+		"password" : 'mejorenelmundo',
+		"profile" : {
+			"copiaPass":'nada',
+			"nombres" : 'JOSE',
+			"apellidoP" :'CRUZ',
+			"apellidoM" :'CRUZ',
+			"telefono" :'66666666',
+			"rol" :'administrador'
+		}
+	};
+	//console.log(user);
+	
+	Meteor.call('registrarAdmin', user );
  });
